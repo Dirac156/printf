a34f03405e3948bfe06da4afcd9f2faa05089498
Dirac Murairi <diracm8@gmail.com>
2020-10-30 10:40:53 +0200

    fix all errors with betty

--- _printf.c
+++ _printf.c
@@ -1,166 +1,50 @@
-#include <stdarg.h>
-#include <unistd.h>
 #include "holberton.h"
 
-
 /**
- *handle_switch- Gets the length of a string
- *@format: string
- *@arguments: list of all argument
- *Return:Length
- */
-
-int handle_switch(char format, va_list arguments)
-{
-int length = 0;
-switch (format)
-{
-case 'c':
-length += print_char(arguments);
-break;
-case 's':
-length += print_str(arguments);
-break;
-case '%':
-write(1, &("%"), 1);
-length++;
-break;
-case 'd':
-length += print_number(arguments);
-break;
-case 'i':
-length += print_number(arguments);
-break;
-}
-return (length);
-}
-
-/**
- *print_str- A function that prints a string
- *@arg:The string
- *Return: number of strings
- */
-int print_str(va_list arg)
-{
-char *s;
-int i = 0;
-s = va_arg(arg, char *);
-if (s == NULL)
-{
-s = "(null)";
-}
-while (s[i] != '\0')
-{
-_putchar(s[i]);
-i++;
-}
-return (i);
-}
-
-/**
- *print_char- A function that prints a character
- *@arg:The list that hold the character
- *Return: number of strings
- */
-int print_char(va_list arg)
-{
-char c;
-c = va_arg(arg, int);
-_putchar(c);
-return (1);
-}
-
-/**
- *print_number- A function that prints integer
- *@arg: The integer
- *Return: number of integer
- */
-int print_number(va_list arg)
-{
-int d = va_arg(arg, int);
-int number;
-int lastdigit = d % 10;
-int one_dig;
-int mult = 1;
-int i = 1;
-
-d = d / 10;
-number = d;
-
-if (lastdigit < 0)
-{
-_putchar('-');
-number = -number;
-d = -d;
-lastdigit = -lastdigit;
-i++;
-}
-if (number > 0)
-{
-while (number / 10 != 0)
-{
-mult = mult * 10;
-number = number / 10;
-}
-number = d;
-while (mult > 0)
-{
-one_dig = number / mult;
-_putchar(one_dig + '0');
-number = number % mult;
-mult = mult / 10;
-i++;
-}
-}
-_putchar(lastdigit + '0');
-
-return (i);
-}
-
-/**
- *_printf- Prints anything to output
- *@format:Parameter 1
- *@...:Variable number of arguements
- *Return: Length
- */
-
+  *_printf - display something on the output screen
+  *@format: the sring that we are going to print
+  *Return: The length og the string
+  */
 int _printf(const char *format, ...)
 {
-int length = 0, checkflag = 0, i = 0;
-va_list arguments;
-char flag[] = {'c', 's', 'd', 'i', '%'};
-va_start(arguments, format);
+int i = 0, j, executed = 0;
+va_list string;
+printer_t funcs[] = {{'c', pr_char}, {'i', pr_int}, {'d', pr_uns},
+{'s', pr_str}, {'%', pr_per}, {'b', pr_bin}};
+va_start(string, format);
 if (format == NULL)
 return (-1);
-while (*format)
+while (format != NULL && *format)
 {
-if (*format != '%')
+if (*format != '%' && format != NULL && *format)
 {
-if (write(1, format, 1) == -1)
-return (-1);
-length++;
-}
-else
-{
+_putchar(*format);
 format++;
-if (!*format)
-return (-1);
-while (flag[i] != '\0')
-{
-if (*format == flag[i++])
-checkflag = 1;
+i++;
 }
-if (checkflag == 1)
-length += handle_switch(*format, arguments);
-else
+else if (*format == '%' && format != NULL && *format)
+{
+if (!*(format + 1))
+return (-1);
+format++, j = 0, executed = 0;
+while (funcs[j].symbol)
+{
+if (*format == funcs[j].symbol)
+{
+i += funcs[j].print(string);
+executed = 1;
+}
+j++;
+}
+if (executed == 0)
 {
 _putchar('%');
 _putchar(*format);
-length++;
-length++;
-}
+i += 2;
 }
 format++;
 }
-return (length);
+}
+va_end(string);
+return (i);
 }
--- holberton.h
+++ holberton.h
@@ -3,11 +3,24 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdio.h>
+/**
+ * struct printer - A new struct type defining a printer.
+ * @symbol: A symbol representing a data type.
+ * @print: A function pointer to a function that prints
+ *a  data type corresponding to symbol.
+ */
+typedef struct printer
+{
+char symbol;
+int (*print)();
+} printer_t;
 int _putchar(char c);
-int _strlen(char *s);
 int _printf(const char *format, ...);
-int print_str(va_list arg);
-int print_char(va_list arg);
-int print_number(va_list arg);
-int handle_switch(char format, va_list arguments);
+int pr_char(va_list arg);
+int pr_int(va_list arg);
+int pr_uns(va_list arg);
+int pr_str(va_list arg);
+int pr_per(void);
+int pr_bin(va_list arg);
+int return_binary(int d);
 #endif
