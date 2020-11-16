e8d1ddcd016a7d1f470aff358da4518e1d203dbd
ISAIAH-max <mlewaisaiah@gmail.com>
2020-11-16 13:21:29 +0300

    Binary functions file

--- binary_flag.c
+++ binary_flag.c
@@ -7,20 +7,20 @@
  */
 int pr_bin(va_list arg)
 {
-int d = va_arg(arg, int);
-int i = 0;
-if (d < 0)
-{
-return (-1);
-}
-else if (d == 0)
-{
-_putchar(0 + '0');
-_putchar(0 + '0');
-}
-else
-i += return_binary(d);
-return (i);
+	int d = va_arg(arg, int);
+	int i = 0;
+	if (d < 0)
+	{
+		return (-1);
+	}
+	else if (d == 0)
+	{
+		_putchar(0 + '0');
+		_putchar(0 + '0');
+	}
+	else
+		i += return_binary(d);
+	return (i);
 }
 
 /**
@@ -31,16 +31,16 @@ return (i);
 
 int return_binary(int d)
 {
-int i = 0;
-if (d == 1)
-{
-_putchar(d + '0');
-return (1);
-}
-else if (d > 1)
-{
-i += return_binary(d / 2);
-_putchar(d % 2 + '0');
-}
-return (i);
+	int i = 0;
+	if (d == 1)
+	{
+		_putchar(d + '0');
+		return (1);
+	}
+	else if (d > 1)
+	{
+		i += return_binary(d / 2);
+		_putchar(d % 2 + '0');
+	}
+	return (i);
 }
